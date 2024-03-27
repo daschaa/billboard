d9aad66e6075cc9abd9730c297d91317cdf89797
daschaa <joshua.weber1@volkswagen.de>
2024-03-27 09:37:09 +0100

    Initial Commit

---
+++ billboard/billboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  billboardApp.swift
+//  billboard
+//
+//  Created by Weber, Joshua (K-DPXQ/5) on 27.03.24.
+//
+
+import SwiftUI
+
+@main
+struct billboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
